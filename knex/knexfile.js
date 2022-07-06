e54f92eba85349d0cc1e76e1fe17dee23c541a39
KenTay7 <johnkta7@gmail.com>
2022-07-06 10:41:45 +0200

    Fix db name

--- knex/knexfile.js
+++ knex/knexfile.js
@@ -5,7 +5,7 @@ module.exports =  {
             host : process.env.DB_HOST,
             user : process.env.DB_USER,
             password : process.env.DB_PASSWORD,
-            database : process.env.DB_DATABASE
+            database : process.env.DB_NAME
         },
         migrations: {
             directory: __dirname + '/knex/migrations'
